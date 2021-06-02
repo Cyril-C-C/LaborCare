c170146c53377cbb29897d2727b118f1b23de580
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-06-02 08:47:22 +0100

    Progressive Changes

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //alert('LaborCare')
+
+// JavaScript for Toggle Menu
+
+  var navLinks = document.getElementById("navLinks");
+
+  function showMenu() {
+    navLinks.style.right = "0";
+  }
+  function hideMenu() {
+    navLinks.style.right = "-12.5rem";
+  }
